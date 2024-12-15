0b1418e94e4385a9a29302269c4c5e0aa1e790f0
SeanFuXiao <fuxiaosean@gmail.com>
2024-12-15 16:23:15 -0500

    set models

---
+++ models/BillModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const BillSchema = new mongoose.Schema({
+    trip_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Trip', required: true },
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: { type: String },
+});
+
+module.exports = mongoose.model('Bill', BillSchema);
